26ae4972adfb01b688bab0093ebd4c1dcea02c3b
Ilya Tsygankov <pegas19962010@gmail.com>
2024-03-09 07:03:12 +0300

    fix form validation on change + root css

--- src/Components/Inventory/InventoryForm/InventoryForm.jsx
+++ src/Components/Inventory/InventoryForm/InventoryForm.jsx
@@ -1,32 +1,41 @@
 import Section from '../../UI/Section';
 import Button from '../../UI/Button';
 import InventoryList from '../InventoryList/InventoryList';
-import { useRef } from 'react';
+import { useState } from 'react';
 import style from './form.module.scss';
 
 const InventoryForm = ({ onSubmit, data, deleteUser }) => {
-	const inputDataRef = useRef();
+	const [inputData, setInputData] = useState('');
+
+	const validInputData = inputData.trim() !== '';
+	let isFormValid = false;
+
+	if (validInputData) {
+		isFormValid = true;
+	}
+
+	const inputChangeHandler = (e) => {
+		setInputData(e.target.value);
+	};
 
 	const sendForm = (e) => {
 		e.preventDefault();
-		const currentInput = inputDataRef.current;
-		if (currentInput.value.trim().length === 0) {
-			currentInput.focus();
-			return;
-		}
-		onSubmit(currentInput.value);
-		currentInput.value = '';
-		currentInput.focus();
+		onSubmit(inputData);
+		setInputData('');
 	};
 
 	return (
 		<Section className={style['form-wrapper']}>
 			<h2>Инвентарь</h2>
 			<form onSubmit={sendForm} className={style.form}>
-				<input type="text" placeholder='Вещь' ref={inputDataRef} />
-				<Button type='submit'
-					onClick={sendForm}
-				>Добавить</Button>
+				<input type="text"
+					placeholder='Вещь'
+					onChange={inputChangeHandler}
+					value={inputData}
+				/>
+				<Button type='submit' onClick={sendForm} disabled={!isFormValid}>
+					Добавить
+				</Button> :
 			</form>
 			<InventoryList data={data} deleteUser={deleteUser} />
 		</Section>
